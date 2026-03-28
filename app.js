5601b931de77494f3d188e7ec4fa6a762eecad69
ketan <ketandabre1@gmail.com>
2026-03-28 13:44:59 +1100

    Added the index route which opens the show route for every listing

--- app.js
+++ app.js
@@ -1,44 +1,48 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const mongo_db = "mongodb://127.0.0.1:27017/wanderlust";
-const Listing = require('./model/listing');
+const Listing = require("./model/listing");
+const path = require("path");
 
+main()
+  .then(() => {
+    console.log("Database connection successful");
+  })
+  .catch((err) => {
+    console.error("Database connection error:", err);
+  });
 
-main().then(() => {
-      console.log('Database connection successful');
-}).catch(err => {
-      console.error('Database connection error:', err);
-}
-);
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
-
-
-
-app.get('/', (req, res) => {
-      res.send('Hello World!');
-}
-);
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
 async function main() {
   await mongoose.connect(mongo_db);
   console.log("Connected to MongoDB");
 }
-
-app.get("/testlistings", async (req, res) => {
-  let sampleListig = new Listing({
-    title: "Test Listing",
-    description: "This is a test listing.",
-    price: 1200,
-    location: "Calangute, Goa",
-    country: "India",
-  });
-  await sampleListig.save();
-      res.send("Test listing created and saved to database!");
-      // res.send("Sample was saved to the database!");
+//index route
+app.get("/listings", async (req, res) => {
+  const allListings = await Listing.find({});
+  res.render("listings/index.ejs", { allListings });
 });
 
+// app.get("/testlistings", async (req, res) => {
+//   let sampleListig = new Listing({
+//     title: "Test Listing",
+//     description: "This is a test listing.",
+//     price: 1200,
+//     location: "Calangute, Goa",
+//     country: "India",
+//   });
+//   await sampleListig.save();
+//       res.send("Test listing created and saved to database!");
+//       // res.send("Sample was saved to the database!");
+// });
 
 app.listen(8080, () => {
-    console.log('Server is running on port 8080');
+  console.log("Server is running on port 8080");
 });
